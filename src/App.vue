70029c2f8039c63ce8b83e01a4272d201db523ef
Алина <nevkaa@yandex.ru>
2018-11-27 15:06:29 +0300

    b5 - 7

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div>
     <h2 v-colored:background.font="'green'">{{title}}</h2>
     <h2 v-colored:color.font.delay="'red'">{{title}}</h2>
+    <h2 v-font>Local fon directive</h2>
   </div>
 </template>
 
@@ -11,6 +12,13 @@
     data() {
       return {
           title: 'Hello'
+      }
+    },
+    directives: {
+      font: {
+        bind(el, binding, vnode) {
+          el.style.fontSize = '40px';
+        }
       }
     }
   }
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import ColorDirective from './color'
 
-Vue.directive('colored', ColorDirective);
+Vue.directive('colored', ColorDirective); // глобальные директивы для всего проекта
 
 new Vue({
   el: '#app',
