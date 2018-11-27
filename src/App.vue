bc65bf9ade97e3375185e43d259ac42c72b4ec37
Алина <nevkaa@yandex.ru>
2018-11-27 19:40:03 +0300

    b8 - 2

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,28 @@
     <form class="pt-3">
       <div class="form-group">
         <label for="email">Email</label>
-        <input type="email" id="email" class="form-control" v-model="email">
+        <input type="email" id="email" class="form-control" @blur="$v.email.$touch()" v-model="email">
       </div>
+
+      <pre>{{$v.email}}</pre>
     </form>
   </div>
 </template>
 
 
 <script>
+  import { required, email } from 'vuelidate/lib/validators'
+
   export default {
     data(){
       return {
         email: ''
+      }
+    },
+    validations: {
+      email: {
+        required,
+        email
       }
     }
   }
