10c164ef00cb696abe606ed4f4e6f40daaff09c9
Алина <nevkaa@yandex.ru>
2018-11-27 14:45:09 +0300

    b5 - 5

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <h2 v-colored="'green'">{{title}}</h2>
+    <h2 v-colored:background="'green'">{{title}}</h2>
+    <h2 v-colored:color="'red'">{{title}}</h2>
   </div>
 </template>
 
--- src/color.js
+++ src/color.js
@@ -1,5 +1,6 @@
 export default {
   bind(el, bindings, vnode){
-    el.style.color = bindings.value;
+    const arg = bindings.arg;
+    el.style[arg] = bindings.value;
   }
 }
