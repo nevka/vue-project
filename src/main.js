b99c1106d5b9baa740892fcd47dfb16bd4c8fcc9
Алина <nevkaa@yandex.ru>
2018-11-28 15:47:07 +0300

    b10 - 1

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import App from './App.vue'
-import VueRouter from 'vue-router'
-import router from './routes'
 
-Vue.use(VueRouter);
+Vue.use(VueResource);
 
 new Vue({
   el: '#app',
-  render: h => h(App),
-  router
+  render: h => h(App)
 });
