f0cf88972c86fa612a8db1666cb4994035ed4c12
Алина <nevkaa@yandex.ru>
2018-11-28 21:13:48 +0300

    b11 - 5

--- src/store/index.js
+++ src/store/index.js
@@ -12,6 +12,14 @@ export default new Vuex.Store({
             state.counter += payload
         }
     },
+    actions: { // здесь вся основная логика (ДБ и т.д.)
+        asyncChangeCounter({commit}, payload) { // context, payload
+            setTimeout(() => {
+                //context.commit('changeCounter', payload.counterValue);
+                commit('changeCounter', payload.counterValue);
+            }, payload.timeoutDelay);
+        }
+    },
     getters: {
         computedCounter(state) {
             return state.counter * 25;
