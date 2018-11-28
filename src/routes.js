6081f2f02bc55a4e21db4fde1b54cec2211baf93
Алина <nevkaa@yandex.ru>
2018-11-28 14:22:21 +0300

    b9 - 12

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,20 @@
 import VueRouter from 'vue-router'
 import Home from './pages/Home';
-import Cars from './pages/Cars';
-import Car from './pages/Car';
+// import Cars from './pages/Cars';
+// import Car from './pages/Car';
 import CarFull from './pages/CarFull';
 import ErrorCmp from './pages/Error';
+
+const Cars = resolve => {
+  require.ensure(['./pages/Cars'], () => {
+      resolve(require('./pages/Cars'));
+  })
+};
+const Car = resolve => {
+  require.ensure(['./pages/Car'], () => {
+      resolve(require('./pages/Car'));
+  })
+};
 
 export default new VueRouter ({
     routes: [
