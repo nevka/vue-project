b120e455fc3809c39bbe9cceadc9d18f53fb99b5
Алина <nevkaa@yandex.ru>
2018-11-28 16:21:21 +0300

    b10 - 4

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,8 @@
       return {
         carName: '',
         carYear: 2018,
-        cars: []
+        cars: [],
+        resource: null
       }
     },
     methods: {
@@ -35,24 +36,31 @@
           name: this.carName,
           year: this.carYear
         };
-        this.$http.post('http://localhost:3000/cars', car)
+        /*this.$http.post('http://localhost:3000/cars', car)
           .then(response => {
             // console.log(response);
             return response.json();
           })
           .then(newCar => {
             console.log(newCar);
-          });
+          });*/
+        this.resource.save({}, car);
       },
       loadCars() {
-        this.$http.get('http://localhost:3000/cars')
+        /*this.$http.get('http://localhost:3000/cars')
           .then(response => {
             return response.json();
           })
           .then(cars => {
               this.cars = cars;
-          });
+          });*/
+        this.resource.get()
+          .then(response => response.json())
+          .then(cars => this.cars = cars);
       }
+    },
+    created() {
+      this.resource = this.$resource('http://localhost:3000/cars');
     }
   }
 </script>
