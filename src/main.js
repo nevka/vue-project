d66fb5d3d515a75df58b9d74c1af3fa05a5b2a61
Алина <nevkaa@yandex.ru>
2018-11-28 10:05:39 +0300

    b9 - 1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import Vuelidate from 'vuelidate'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
-Vue.use(Vuelidate);
+Vue.use(VueRouter);
 
 new Vue({
   el: '#app',
