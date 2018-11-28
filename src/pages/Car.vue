6fc3f2134db0120088c01f76a3d063ce6f70dc61
Алина <nevkaa@yandex.ru>
2018-11-28 11:14:04 +0300

    b9 - 7

--- src/pages/Car.vue
+++ src/pages/Car.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
     <h1>Car.vue {{id}}</h1>
-    <button class="btn btn-sm btn-info" @click="goBackToCars">Back</button>
+    <button class="btn btn-sm btn-defaul mb-2" @click="goBackToCars">Back</button>
+    <br>
+    <!--<router-link class="btn btn-info mt-2" tag="button" :to="'/car/'+id+'/full'">Full info</router-link>-->
+    <router-link class="btn btn-info mt-2" tag="button" :to="{name: 'carFull', params: {id: id}}">Full info</router-link>
+
+    <hr>
+    <router-view></router-view>
   </div>
 </template>
 
---
+++ src/pages/CarFull.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>Car name: Ford</h2>
+    <h2>Car year: 2015</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+
+</style>
